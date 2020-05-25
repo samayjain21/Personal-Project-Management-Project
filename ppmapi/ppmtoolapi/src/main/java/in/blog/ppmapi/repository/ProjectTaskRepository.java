bb0d67b1adf51ffca45ba3a2a98f73d45bb738dc
samayjain21 <63366920+samayjain21@users.noreply.github.com>
2020-05-26 02:20:35 +0530

    backlog and projectTask domain, repository, service and controller added

---
+++ ppmapi/ppmtoolapi/src/main/java/in/blog/ppmapi/repository/ProjectTaskRepository.java
@@ -0,0 +1,9 @@
+package in.blog.ppmapi.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import in.blog.ppmapi.domain.ProjectTask;
+@Repository
+public interface ProjectTaskRepository extends CrudRepository<ProjectTask, Long> {
+
+}
