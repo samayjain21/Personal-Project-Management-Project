c587abe8f90f1cfdb01d0a3a36c376405913e853
samayjain21 <63366920+samayjain21@users.noreply.github.com>
2020-06-01 14:06:15 +0530

    onSubmit and error handling in project task

--- ppmcli/src/actions/BacklogActions.js
+++ ppmcli/src/actions/BacklogActions.js
@@ -1,6 +1,19 @@
 import axios from "axios";
-
-export const addProjectTask = (backlog_id, project_task,history) => async( dispatch )=>{
-    await axios.post(`/api/backlog/${backlog_id},project_task`);
+import { GET_ERRORS } from "./type";
+export const addProjectTask = (backlog_id, project_task, history) => async (
+  dispatch
+) => {
+  try {
+    await axios.post(`/api/backlog/${backlog_id}`, project_task);
     history.push(`/projectBoard/${backlog_id}`);
+    dispatch({
+      type: GET_ERRORS,
+      payload: {},
+    });
+  } catch (err) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data,
+    });
+  }
 };
